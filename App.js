ae05f005bf9c0aeff86e2433737e2f26f6ceed0b
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2019-11-26 01:24:47 -0300

    Integração do Firebase e FirebaseAuth

--- App.js
+++ App.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
-export default function App() {
-  return (
-    <View>
-      <Text>App Barber</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  register = async () => {
+    const { email, password } = this.state;
+
+    try {
+      console.log('email', email);
+      console.log('senha', password);
+      const resp = await auth().createUserWithEmailAndPassword(email, password);
+      console.log('resp', resp);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <View>
+        <Text>App Barber</Text>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={value => this.setState({ email: value })}
+        />
+        <TextInput
+          placeholder="Senha"
+          value={password}
+          onChangeText={value => this.setState({ password: value })}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            this.register();
+          }}
+        >
+          <Text>Criar Usuário</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
