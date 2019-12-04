fc53a80cbfa560ea651598b0424beda32f374b8c
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2019-12-03 23:34:26 -0300

    StatusBar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-// import {StatusBar} from 'react-native';
+import { StatusBar } from 'react-native';
 import Routes from './routes';
 
 import './config/ReactotronConfig';
@@ -12,6 +12,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#ffff" />
         <Routes />
       </PersistGate>
     </Provider>
