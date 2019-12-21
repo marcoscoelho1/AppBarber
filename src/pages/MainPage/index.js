15155959e3576971b51e2d93aa3091346e770075
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2019-12-20 23:58:06 -0300

    Adição do mapa

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,25 +1,117 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
+// import MapView from 'react-native-maps';
+import { Text } from 'react-native';
 import { logOut } from '~/store/modules/auth/actions';
 import Button from '~/components/Button';
 
-function MainPage({ logOut, navigation }) {
-  return (
-    <View>
-      <Text>Página Principal</Text>
-      <Button
-        onPress={() => {
-          logOut();
-          navigation.navigate('Login');
-        }}
-      >
-        Sair
-      </Button>
-    </View>
-  );
+import {
+  Container,
+  MapContainer,
+  ViewMap,
+  Header,
+  BarberShopDetails,
+  mapStyle,
+} from './styles';
+
+export class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      detailsVisible: false,
+      barbershopSelected: {
+        name: '',
+        address: '',
+        description: '',
+      },
+    };
+  }
+
+  setBabershopSelected = babershop => {
+    this.setState({
+      barbershopSelected: { ...babershop },
+    });
+    this.handleShowDetailsVisible();
+  };
+
+  handleShowDetailsVisible = () => {
+    this.setState({
+      detailsVisible: true,
+    });
+  };
+
+  handleCloseDetailsVisible = () => {
+    this.setState({
+      detailsVisible: false,
+    });
+  };
+
+  render() {
+    const { navigation, logOut } = this.props;
+    const { detailsVisible, barbershopSelected } = this.state;
+
+    return (
+      <Container>
+        <Header>
+          <Button
+            onPress={() => {
+              logOut();
+              navigation.navigate('Login');
+            }}
+          >
+            Sair
+          </Button>
+        </Header>
+        <MapContainer>
+          <ViewMap
+            customMapStyle={mapStyle}
+            loadingEnabled
+            region={{
+              latitude: -23.1910636,
+              longitude: -47.3139639,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            }}
+            onPress={() => {
+              this.handleCloseDetailsVisible();
+            }}
+          >
+            <ViewMap.Marker
+              coordinate={{ latitude: -23.1929053, longitude: -47.3124952 }}
+              // title="title"
+              // description="description"
+              onPress={() => {
+                this.setBabershopSelected({
+                  name: 'Barbearia 1',
+                  address: 'Rua Europa',
+                  description: 'Barbearia Top',
+                });
+              }}
+            />
+            <ViewMap.Marker
+              coordinate={{ latitude: -23.1910685, longitude: -47.3117752 }}
+              onPress={() => {
+                this.setBabershopSelected({
+                  name: 'Barbearia 2',
+                  address: 'Rua Europa',
+                  description: 'Barbearia Top',
+                });
+              }}
+            />
+          </ViewMap>
+          {detailsVisible && (
+            <BarberShopDetails>
+              <Text>{barbershopSelected.name}</Text>
+              <Text>{barbershopSelected.address}</Text>
+              <Text>{barbershopSelected.description}</Text>
+            </BarberShopDetails>
+          )}
+        </MapContainer>
+      </Container>
+    );
+  }
 }
 
 MainPage.propTypes = {
